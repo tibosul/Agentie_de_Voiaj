e5239688ebbb3fa8f9492fc7e8907995b071fb03
Stelian Tiberiu-Andrei <steliantiberiuandrei2005@gmail.com>
2025-08-11 14:13:28 +0300

    Add project files.

---
+++ Agentie_de_Voiaj_Client/main_window.cpp
@@ -0,0 +1,11 @@
+#include "main_window.h"
+
+Main_Window::Main_Window(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
+
+Main_Window::~Main_Window()
+{}
+
---
+++ Agentie_de_Voiaj_Server/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, Agentie de Voiaj Server!" << std::endl;
+	return 0;
+}
